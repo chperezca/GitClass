257524fc24b5aa2bd0460af0e3115e2b61cb5c53
global <chperezca@outlook.com>
2023-09-12 16:13:13 -0500

    Trying to add updating (New information be updated)

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -4,15 +4,9 @@ import java.util.Iterator;
 
 public class Testing {
 	public static void main(String[] args) {	
-		System.out.println(isValid("[]{}"));
 		
-		 
 	}
-	
-	    public static boolean isValid(String s) {
-	    	
-	    	return false;	    	
-	    }
-	}
+		    
+}
 	
 
